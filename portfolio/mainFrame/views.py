452b6627b3d05434c51dded5530588bf0bde0181
Kai Jie Chow <kj1chow@gmail.com>
2021-06-13 01:41:43 +0100

    13-6 progress

--- portfolio/mainFrame/views.py
+++ portfolio/mainFrame/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 
 def mainFrame(request, name="kai jie"):
+    user = True if name == "kai jie" else False
     return render(request, "mainFrame/mainFrame.html", {
         "name": name.title(),
+        "user": user,
     })

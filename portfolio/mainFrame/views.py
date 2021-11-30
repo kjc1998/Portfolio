096f3a3ac55fba7b74fd8303c9ae5eb077c63343
Kai Jie Chow <kj1chow@gmail.com>
2021-11-30 22:54:33 +0000

    location removed, pdf handling with base64 completed

--- portfolio/mainFrame/views.py
+++ portfolio/mainFrame/views.py
@@ -1,10 +1,16 @@
 import os
+import json
+import base64
 from django.shortcuts import render
 
 
 def mainFrame(request, name="kai jie"):
     user = True if name == "kai jie" else False
+    cv_file = open(
+        "C:/Users/User/Desktop/Coding_Projects/pythonProjects/Portfolio/portfolio/media/cv/CV_KaiJieChow.pdf", "rb").read()
+    cv_encode = base64.b64encode(cv_file).decode("utf-8")
     return render(request, "mainFrame/mainFrame.html", {
         "name": name.title(),
         "user": user,
+        "cv_encode": cv_encode,
     })

a070f522ac84ee23a039450558e4069b6e4f1ecd
Kai Jie Chow <kj1chow@gmail.com>
2021-12-24 13:58:51 +0000

    comments and changes to full screen pdf handling

--- portfolio/cv/views.py
+++ portfolio/cv/views.py
@@ -1,10 +1,15 @@
 import os
+import base64
 from django.shortcuts import render
 
 
 def cvManagement(request, name="kai jie"):
     user = True if name == "kai jie" else False
+    cv_file = open(
+        "C:/Users/User/Desktop/Coding_Projects/pythonProjects/Portfolio/portfolio/media/cv/CV_KaiJieChow.pdf", "rb").read()
+    cv_encode = base64.b64encode(cv_file).decode("utf-8")
     return render(request, "cv/cvManagement.html", {
         "name": name.title(),
         "user": user,
+        "cv_encode": cv_encode,
     })

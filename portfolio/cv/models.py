46f57c6f4b20a5209227ea120553ba49dff0ce13
Kai Jie Chow <kj1chow@gmail.com>
2021-12-26 13:07:29 +0000

    database setup for cv upload

--- portfolio/cv/models.py
+++ portfolio/cv/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CV_VERSION(models.Model):
+    date = models.DateField(null=False)
+    data = models.FileField()
+    active = models.BooleanField()

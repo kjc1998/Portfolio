5de2ef06ae7c3ef71a77f0d66c7586497a9e6bdf
Kai Jie Chow <kj1chow@gmail.com>
2021-12-29 15:49:41 +0000

    models for project handling completed

--- portfolio/project/models.py
+++ portfolio/project/models.py
@@ -8,14 +8,22 @@ class Project(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100, unique=True)
     start_date = models.DateField(null=False)
-    end_date = models.DateField()
-    ongoing = models.BooleanField()
+    end_date = models.DateField(null=True)
+    ongoing = models.BooleanField(null=True)
 
 
 class Story(models.Model):
     id = models.IntegerField(primary_key=True)
     date = models.DateField(null=False)
     content = models.CharField(max_length=5000)
-    primary_image = models.BinaryField()
+    primary_image = models.BinaryField(null=True)
     project = models.ForeignKey(
         Project, on_delete=models.CASCADE, related_name="story")
+
+
+class Tags(models.Model):
+    # Tags with Many to Many Relationship
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    project = models.ManyToManyField(Project, related_name="tags")
+    story = models.ManyToManyField(Story, related_name="tags")

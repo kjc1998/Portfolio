57c4784811745eabbb744fb2c904df24afd5a274
kchow <kchow@illumina.com>
2022-03-11 20:53:43 +0000

    navigation to story page

--- portfolio/project/views.py
+++ portfolio/project/views.py
@@ -9,52 +9,58 @@ from .models import Project, Story, Tags
 
 def projectManagement(request):
     if request.method == "POST":
-        if "editProject" in request.POST:
-            field_entries = request.POST
-            project_id = field_entries["projectID"]
-            name = field_entries["projectName_"+project_id]
-            start = datetime.strptime(
-                field_entries["projectStart_"+project_id], "%Y-%m-%d")
-            end = datetime.strptime(
-                field_entries["projectEnd_"+project_id], "%Y-%m-%d")
-
-            edit_project = Project.objects.filter(id=int(project_id)).first()
-            edit_project.name = name
-            edit_project.start_date = start
-            try:
-                # Check Input
-                ongoing = field_entries["projectOngoing_"+project_id]
-                edit_project.end_date = datetime.now()  # Default to Current Date
-                edit_project.ongoing = True
-            except MultiValueDictKeyError:
-                edit_project.end_date = end
-                edit_project.ongoing = False
-            edit_project.save()
-        elif "deleteProject" in request.POST:
-            delete_project = Project.objects.filter(
-                id=int(request.POST["deleteProject"])).first()
-            delete_project.delete()
-        elif "addProject" in request.POST:
-            try:
+        if request.user.is_authenticated:
+            if "editProject" in request.POST:
                 field_entries = request.POST
-                name = field_entries["projectName_new"]
+                project_id = field_entries["projectID"]
+                name = field_entries["projectName_"+project_id]
                 start = datetime.strptime(
-                    field_entries["projectStart_new"], "%Y-%m-%d")
+                    field_entries["projectStart_"+project_id], "%Y-%m-%d")
                 end = datetime.strptime(
-                    field_entries["projectEnd_new"], "%Y-%m-%d")
+                    field_entries["projectEnd_"+project_id], "%Y-%m-%d")
+
+                edit_project = Project.objects.filter(id=int(project_id)).first()
+                edit_project.name = name
+                edit_project.start_date = start
                 try:
-                    ongoing = field_entries["projectOngoing_new"]
-                    ongoing = True
+                    # Check Input
+                    ongoing = field_entries["projectOngoing_"+project_id]
+                    edit_project.end_date = datetime.now()  # Default to Current Date
+                    edit_project.ongoing = True
                 except MultiValueDictKeyError:
-                    ongoing = False
-                new_project = Project(
-                    name=name, start_date=start, end_date=end, ongoing=ongoing)
-                new_project.save()
-            except IntegrityError:
-                return render(request, "defaultError.html", {
-                    "error_status": 422,
-                    "error_message": "This file already existed",
-                }, status=422)
+                    edit_project.end_date = end
+                    edit_project.ongoing = False
+                edit_project.save()
+            elif "deleteProject" in request.POST:
+                delete_project = Project.objects.filter(
+                    id=int(request.POST["deleteProject"])).first()
+                delete_project.delete()
+            elif "addProject" in request.POST:
+                try:
+                    field_entries = request.POST
+                    name = field_entries["projectName_new"]
+                    start = datetime.strptime(
+                        field_entries["projectStart_new"], "%Y-%m-%d")
+                    end = datetime.strptime(
+                        field_entries["projectEnd_new"], "%Y-%m-%d")
+                    try:
+                        ongoing = field_entries["projectOngoing_new"]
+                        ongoing = True
+                    except MultiValueDictKeyError:
+                        ongoing = False
+                    new_project = Project(
+                        name=name, start_date=start, end_date=end, ongoing=ongoing)
+                    new_project.save()
+                except IntegrityError:
+                    return render(request, "defaultError.html", {
+                        "error_status": 422,
+                        "error_message": "This file already existed",
+                    }, status=422)
+        else:
+            return render(request, "defaultError.html", {
+                "error_status": 403,
+                "error_message": "Please login to enter this site",
+            }, status=403)
 
     # Update Ongoing Project with Time
     ongoing_projects = Project.objects.filter(ongoing=True).all()
@@ -91,59 +97,65 @@ def projectMain(request, pid):
             "error_message": "No such project exists",
         }, status=500)
     if request.method == "POST":
-        if "storyNew_add" in request.POST:
-            field_entries = request.POST
-            name = str(field_entries["storyName_new"])
-            date = datetime.strptime(field_entries["storyDate_new"], "%Y-%m-%d")
-            tags = [tag.strip() for tag in field_entries["storyTags_final"].split(",")]
-            content = str(field_entries["storyContent_new"])
-            try:
-                image_bytes_data = request.FILES["storyImage_new"].file.read()
-            except KeyError:
-                image_bytes_data = None
+        if request.user.is_authenticated:
+            if "storyNew_add" in request.POST:
+                field_entries = request.POST
+                name = str(field_entries["storyName_new"])
+                date = datetime.strptime(field_entries["storyDate_new"], "%Y-%m-%d")
+                tags = [tag.strip() for tag in field_entries["storyTags_final"].split(",")]
+                content = str(field_entries["storyContent_new"])
+                try:
+                    image_bytes_data = request.FILES["storyImage_new"].file.read()
+                except KeyError:
+                    image_bytes_data = None
 
-            # check date (inclusive)
-            if not (date.date() >= current_project.start_date and date.date() <= current_project.end_date):
-                return render(request, "defaultError.html", {
-                    "error_status": 400,
-                    "error_message": f"Date is out of project range. Project Date: {current_project.start_date} till {current_project.end_date}",
-                }, status=400)
-
-            # check image
-            if image_bytes_data:
-                if "image" not in request.FILES["storyImage_new"].content_type:
+                # check date (inclusive)
+                if not (date.date() >= current_project.start_date and date.date() <= current_project.end_date):
                     return render(request, "defaultError.html", {
                         "error_status": 400,
-                        "error_message": f"Invalid file type",
+                        "error_message": f"Date is out of project range. Project Date: {current_project.start_date} till {current_project.end_date}",
                     }, status=400)
 
-            # add new story
-            try:
-                new_story_instance = Story(name=name, date=date, content=content, primary_image=image_bytes_data, project=current_project)
-                new_story_instance.save()
-            except IntegrityError:
-                return render(request, "defaultError.html", {
-                    "error_status": 422,
-                    "error_message": "This story name already existed",
-                }, status=422)
+                # check image
+                if image_bytes_data:
+                    if "image" not in request.FILES["storyImage_new"].content_type:
+                        return render(request, "defaultError.html", {
+                            "error_status": 400,
+                            "error_message": f"Invalid file type",
+                        }, status=400)
 
-            # populating tags
-            current_story = Story.objects.get(name=name)
-            for tag in tags:
-                if tag == "":
-                    break
-
+                # add new story
                 try:
-                    new_tag_instance = Tags(name=tag)
-                    new_tag_instance.save()
+                    new_story_instance = Story(name=name, date=date, content=content, primary_image=image_bytes_data, project=current_project)
+                    new_story_instance.save()
                 except IntegrityError:
-                    pass
-                
-                # tying tags to projects and stories
-                current_tag = Tags.objects.get(name=tag)
-                current_tag.project.add(current_project)
-                current_tag.story.add(current_story)
-    
+                    return render(request, "defaultError.html", {
+                        "error_status": 422,
+                        "error_message": "This story name already existed",
+                    }, status=422)
+
+                # populating tags
+                current_story = Story.objects.get(name=name)
+                for tag in tags:
+                    if tag == "":
+                        break
+
+                    try:
+                        new_tag_instance = Tags(name=tag)
+                        new_tag_instance.save()
+                    except IntegrityError:
+                        pass
+                    
+                    # tying tags to projects and stories
+                    current_tag = Tags.objects.get(name=tag)
+                    current_tag.project.add(current_project)
+                    current_tag.story.add(current_story)
+        else:
+            return render(request, "defaultError.html", {
+                "error_status": 403,
+                "error_message": "Please login to enter this site",
+            }, status=403)
+
     # pagination based on chronological order
     stories = current_project.story.all().order_by("-date")
     story_paginator = Paginator(stories, 5)
@@ -164,4 +176,11 @@ def projectMain(request, pid):
     })
 
 def storyMain(request, pid, sid):
+    try:
+        current_story = Story.objects.get(id=sid, project=pid)
+    except Story.DoesNotExist:
+        return render(request, "defaultError.html", {
+            "error_status": 500,
+            "error_message": "No such Project/Story",
+        }, status=500)
     return render(request, "mainFrame/mainFrame.html")
